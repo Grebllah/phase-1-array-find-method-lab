33cd914a16f1b5156a5d0f163b0370aa6f36d649
James Hallberg <Grebllah81788@gmail.com>
2023-06-14 15:35:07 -0400

    complete array find lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function winYear(record){
+    return record.result === "W"
+}
+const superbowlWin = (year) => {
+    const yearW = year.find(winYear)
+    if (yearW) {
+        return yearW.year
+    }
+}
